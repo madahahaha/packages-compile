6b736c36d8f1ff24eff6836c68d44e46d7a49112
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-01-01 18:56:36 +0800

    update

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -1,3 +1,1 @@
-sed -i '/+docker-ce /d' package/feeds/custom/luci-app-dockerman/Makefile
 sed -i 's/+libcap /+libcap +libcap-bin /' package/feeds/custom/luci-app-openclash/Makefile
-rm -Rf package/feeds/custom/luci-theme-*/root/etc/uci-defaults
