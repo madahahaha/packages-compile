fca6dd2cb6a9686406a879af2ccdc1f20acc8a78
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-09 04:35:54 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -2,7 +2,7 @@ for ipk in $(ls package/feeds/custom | grep "luci-app"); do
   	echo "CONFIG_PACKAGE_$ipk=m" >> .config
 done
 
-rm -Rf feeds/luci
+rm -Rf feeds/luci/!(luci.mk)
 rm -Rf feeds/packages/{sound,net,multimedia,mail,admin,ipv6,utils,lang/php*,lang/ruby}
 
 sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
