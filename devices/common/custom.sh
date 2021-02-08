8df8453b95d5c55eeb4096dc1c05414543b05e14
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-08 20:55:38 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -7,8 +7,6 @@ rm -Rf feeds/packages/{sound,net,multimedia,mail,admin,ipv6,utils}
 
 sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
 
-sed -i '/+docker/d' package/feeds/custom/luci-app-dockerman/Makefile
-
 mv -f feeds/packages/net/nlbwmon package/feeds/custom/nlbwmon
 
 rm -Rf package/feeds/packages/ffmpeg
