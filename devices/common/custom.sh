257b2b8df3020ad1e3f4d7f9c42a9e6a7fae55e0
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-08 14:00:13 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -1,3 +1,9 @@
+for ipk in $(ls package/feeds/custom | grep "luci-app"); do	
+  	echo "CONFIG_PACKAGE_$ipk=m" >> .config
+done
+
+find package/feeds/custom -maxdepth 0 -name "luci-app"
+
 sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
 
 sed -i '/+docker/d' package/feeds/custom/luci-app-dockerman/Makefile
