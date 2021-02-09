5345945f104a5a6947cef47e77450f69d3acf936
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-09 14:50:18 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -10,7 +10,7 @@ rm -Rf feeds/luci/!(luci.mk|build|modules)
 rm -Rf feeds/luci/modules/!(luci-base)
 
 rm -Rf feeds/packages/{sound,net,multimedia,mail,admin,ipv6,utils,devel,lang/php*,lang/ruby}
-rm -Rf package/{network,system,utils,firmware,kernel,devel}
+rm -Rf package/{network,utils,devel,base-files,boot,devel,firmware,kernel}
 
 sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
 
