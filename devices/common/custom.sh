83acfa03ed1a51d03af87656728e91a0c3617868
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-09 11:18:25 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -6,7 +6,8 @@ for ipk in $(ls package/feeds/custom | grep "luci-app"); do
   	echo "CONFIG_PACKAGE_$ipk=m" >> .config
 done
 
-rm -Rf feeds/luci/!(luci.mk)
+rm -Rf feeds/luci/!(luci.mk|build|modules)
+rm -Rf feeds/luci/modules/!(luci-base)
 
 rm -Rf feeds/packages/{sound,net,multimedia,mail,admin,ipv6,utils,lang/php*,lang/ruby}
 
