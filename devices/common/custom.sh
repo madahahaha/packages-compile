022c58a43a522e5206176fabc641b3900c4ddfc9
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-13 12:52:52 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -11,7 +11,7 @@ rm -Rf feeds/{routing,telephony,freifunk}
 rm -Rf feeds/luci/!(luci.mk|build|modules)
 rm -Rf feeds/luci/modules/!(luci-base)
 
-rm -Rf feeds/packages/!(lang|libs|utils)
+rm -Rf feeds/packages/!(lang|libs|utils|devel)
 rm -Rf feeds/packages/lang/!(|python|golang)
 rm -Rf feeds/base/package/network
 
