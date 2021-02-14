579bf914b7961cad8beb8032ae6dc0db34f431be
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-14 22:17:56 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -14,6 +14,7 @@ rm -Rf feeds/luci/modules/!(luci-base)
 rm -Rf feeds/packages/!(lang|libs|devel|utils)
 rm -Rf feeds/packages/utils/!(pcsc-lite)
 rm -Rf feeds/packages/lang/!(|python|golang)
+rm package/feeds/base/ppp && mv -f feeds/base/package/network/services/ppp package/feeds/base/
 rm -Rf feeds/base/package/{kernel,network}
 rm -Rf feeds/base/package/system/{rpcd,procd}
 
@@ -21,7 +22,6 @@ rm -Rf feeds/base/package/utils/spidev_test
 
 sed -i 's,$(STAGING_DIR_HOST)/bin/,,' package/feeds/custom/*/Makefile
 
-sed -i "s,PPPD_VER :.*,PPPD_VER := \$(shell curl -s 'https://raw.githubusercontent.com/openwrt/openwrt/master/package/network/services/ppp/Makefile' | grep 'PKG_RELEASE_VERSION:=' | awk -F'=' '{print \$\$2}')," package/feeds/custom/netkeeper*/Makefile
 sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
 
 
