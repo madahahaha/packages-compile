137482b2847bcf095cda25ef24d9b8ce9f36803c
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-15 09:41:34 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -12,14 +12,14 @@ rm -Rf feeds/luci/{applications,collections,protocols,themes,libs}
 rm -Rf feeds/luci/modules/!(luci-base)
 
 rm -Rf feeds/packages/!(lang|libs|devel|utils|net)
-rm -Rf feeds/packages/utils/!(pcsc-lite)
+rm -Rf feeds/packages/utils/!(pcsc-lite|xz)
 rm -Rf feeds/packages/net/!(mosquitto)
 rm -Rf feeds/packages/lang/!(python|golang)
 rm -Rf feeds/base/package/kernel
 rm -Rf feeds/base/package/network/!(services)
 rm -Rf feeds/base/package/network/services/!(ppp)
 rm -Rf feeds/base/package/utils/!(util-linux|lua)
-rm -Rf feeds/base/package/system/{rpcd,procd}
+rm -Rf feeds/base/package/system/!(opkg)
 
 sed -i 's,$(STAGING_DIR_HOST)/bin/upx,upx,' package/feeds/custom/*/Makefile
 
