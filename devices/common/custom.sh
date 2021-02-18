b9e839a372cd1109e954de52de3f9090c36d93a8
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-19 00:44:24 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -2,6 +2,20 @@
 
 shopt -s extglob
 
+sed -i 's/Os/O2/g' include/target.mk
+rm -rf ./feeds/packages/lang/golang
+svn co https://github.com/immortalwrt/packages/trunk/lang/golang feeds/packages/lang/golang
+sed -i "s/+nginx\( \|$\)/+nginx-ssl\1/g"  package/*/*/*/Makefile
+sed -i 's/+python\( \|$\)/+python3/g' package/*/*/*/Makefile
+sed -i 's?../../lang?$(TOPDIR)/feeds/packages/lang?g' package/feeds/custom/*/Makefile
+for ipk in $(find package/feeds/custom/* -maxdepth 0); do	
+	if [[ ! -d "$ipk/patches" && ! "$(grep "codeload.github.com" $ipk/Makefile)" ]]; then
+		find $ipk/ -maxdepth 1 -name "Makefile" \
+		| xargs -i sed -i "s/PKG_SOURCE_VERSION:=[0-9a-z]\{15,\}/PKG_SOURCE_VERSION:=HEAD/g" {}
+	fi	
+done
+sed -i 's/$(VERSION) &&/$(VERSION) ;/g' include/download.mk
+
 rm -rf package/feeds/custom/{luci-app-turboacc,luci-app-mtwifi}
 
 for ipk in $(ls package/feeds/custom | grep "luci-"); do		
