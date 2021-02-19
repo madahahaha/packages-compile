1117101608f11330716f61bad579962105d55630
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-02-20 00:58:49 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -2,7 +2,7 @@
 
 shopt -s extglob
 
-rm -rf feeds/custom/{luci-app-turboacc,luci-app-mtwifi}
+rm -rf feeds/custom/luci-app-{turboacc,mtwifi,frps}
 
 rm -Rf feeds/luci/{applications,collections,protocols,themes,libs}
 rm -Rf feeds/luci/modules/!(luci-base)
