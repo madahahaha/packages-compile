5d43458d2b3d36a5ad411934c4e66f6f03420080
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-05-10 00:58:51 +0800

    Update custom.sh

--- devices/redmi-ac2100/custom.sh
+++ devices/redmi-ac2100/custom.sh
@@ -1,8 +1,3 @@
 # rm -Rf package/feeds/packages/node
 # svn co https://github.com/coolsnowwolf/packages/trunk/lang/node package/feeds/custom/node
 
-echo '
-UnblockNeteaseMusic
-' >> ipk
-
-sed -i '/sub-web/d' ipk
