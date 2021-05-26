d17a7e151e5e796f18cf9d737bdf7cc45f5b7336
garypang13 <48883331+garypang13@users.noreply.github.com>
2021-05-26 14:02:00 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -19,7 +19,7 @@ sed -i 's/+python\( \|$\)/+python3/g' package/feeds/custom/*/Makefile
 sed -i 's?../../lang?$(TOPDIR)/feeds/packages/lang?g' package/feeds/custom/*/Makefile
 for ipk in $(find package/feeds/custom/* -maxdepth 0); do	
 	if [[ ! -d "$ipk/patches" && ! "$(grep "codeload.github.com" $ipk/Makefile)" ]]; then
-		find $ipk/ -maxdepth 1 -name "Makefile" ! -path *tcping* \
+		find $ipk/ -maxdepth 1 -name "Makefile" ! -path *tcping* ! -path *n2n_v2* \
 		| xargs -i sed -i "s/PKG_SOURCE_VERSION:=[0-9a-z]\{15,\}/PKG_SOURCE_VERSION:=HEAD/g" {}
 	fi
 done
