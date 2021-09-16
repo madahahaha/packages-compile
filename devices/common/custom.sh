d5c0b81f3275f621fed4aaddd678383adf7b80bf
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-09-17 04:23:25 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -25,6 +25,7 @@ rm -Rf feeds/base/package/system/!(opkg|ubus|uci)
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+sed -i '/killall -HUP rpcd/d' package/feeds/luci/luci.mk
 sed -i '/page.acl_depends/d' `find package/feeds/custom/luci-*/luasrc/controller/* -name "*.lua"`
 sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/custom/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
 sed -i 's/Os/O2/g' include/target.mk
