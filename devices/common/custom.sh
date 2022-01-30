7f8606d6adec5be6bd55dae99589e00327255f80
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-01-30 12:40:59 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -49,8 +49,7 @@ make defconfig
 rm -Rf tmp
 mv feeds/base.bak feeds/base
 mv feeds/packages.bak feeds/packages
-sed -i -e 's/CONFIG_ALL=y/CONFIG_ALL=n/' \
--e '/CONFIG_SIGNATURE_CHECK/d' .config
+sed -i 's/CONFIG_ALL=y/CONFIG_ALL=n/' .config
 sed -i '/PACKAGE_kmod-/d' .config
 
 sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
