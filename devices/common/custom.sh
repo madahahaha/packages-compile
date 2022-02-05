95633353e3015cb00e53c3f57df5b0b468c93005
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-02-05 16:13:00 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -26,6 +26,7 @@ rm -Rf feeds/base/package/system/!(opkg|ubus|uci)
 ./scripts/feeds install -a -p kiddin9
 ./scripts/feeds install -a
 
+sed -i "s/tty1::askfirst/tty1::respawn/g" target/linux/*/base-files/etc/inittab
 sed -i 's/\(page\|e\)\?.acl_depends.*\?}//' `find package/feeds/kiddin9/luci-*/luasrc/controller/* -name "*.lua"`
 sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/kiddin9/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
 sed -i 's/Os/O2/g' include/target.mk
